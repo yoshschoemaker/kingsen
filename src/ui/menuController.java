c94e37d5c3376cbf0e12884c735b5eb317c07f5d
Yosh Schoemaker <yosh.schoemaker@aceworks.nl>
2019-03-29 10:38:09 +0100

    Menu pushje

--- src/ui/menuController.java
+++ src/ui/menuController.java
@@ -1,38 +1,21 @@
 package ui;
 
 import Kingsen.Controller;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class menuController extends Controller {
 
-    public void givePenaltyClicked(MouseEvent event) throws IOException
+    public void givePenaltyClicked()
     {
-        Parent startViewParent = FXMLLoader.load(getClass().getResource("givePenalty.fxml"));
-        Scene startViewScene = new Scene(startViewParent);
-
-        // This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(startViewScene);
-        window.show();
+        switchScene("givePenalty");
     }
 
-    public void closeMenuClicked(MouseEvent event) throws IOException
+    public void closeMenuClicked()
     {
-        Parent startViewParent = FXMLLoader.load(getClass().getResource("game.fxml"));
-        Scene startViewScene = new Scene(startViewParent);
+        switchScene("game");
+    }
 
-        // This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(startViewScene);
-        window.show();
+    public void quitGameClicked()
+    {
+        System.exit(0);
     }
 }
